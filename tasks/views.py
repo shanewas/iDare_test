ac0afe3ebb4906fda162bededb34dac83f609756
Shanewas Ahmed <shanewasahmed@gmail.com>
2021-08-27 16:23:55 +0600

    view ready test with bootstrap

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'tasks/home.html')
+
+
+def status(request):
+    return render(request, 'tasks/status.html', {'title': 'Status'})
