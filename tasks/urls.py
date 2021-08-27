9c825ace920676700b09cf35869a6acbcadf49b6
Shanewas Ahmed <shanewasahmed@gmail.com>
2021-08-27 16:29:50 +0600

    html templates added

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='tasks-home'),
+    path('', views.home, name='tasks-home'),
     path('status/', views.status, name='tasks-status'),
 ]
